c19a6369e5d59e874c5f8e5eb7ae02b7035c354f
tarik <tarik@MacBook-Air-de-cli.local>
2019-03-14 00:16:10 +0100

    add token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI : 'mongodb+srv://tarik:1234567890@travelgroupe-mmi2h.mongodb.net/test?retryWrites=true'
+    mongoURI : 'mongodb+srv://tarik:1234567890@travelgroupe-mmi2h.mongodb.net/test?retryWrites=true',
+    secretOrKey : 'secret'
 }
