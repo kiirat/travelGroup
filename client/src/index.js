cf580d805aad57fd28e78ac9c3d595a4361903d7
tarik <tarik@MacBook-Air-de-cli.local>
2019-05-17 19:24:45 +0000

    make structure

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'jquery/dist/jquery';
+import 'popper.js/dist/popper.js';
+import 'bootstrap/dist/js/bootstrap';
+// import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
